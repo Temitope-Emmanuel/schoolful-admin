3fa3d34dbb57d5145ba9faa6d9934ca4d14a629e
Temitope Emmanuel OJo <temitopeojo0@gmail.com>
2022-02-09 05:20:43 +0100

    updated the server folder

--- server/parse-apps.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apps : [{
-      name        : "Faithful_Web",
-      script      : "npm start",
-      watch       : true,
-      merge_logs  : true,
-      cwd         : "/var/www/faithful_web/server",
-     }]
-  }
